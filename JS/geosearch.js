1a3338dc1a2c898128c7ee9dbeac3eddfa8c5dda
Zach Austin <zacharyraustin@gmail.com>
2014-05-14 22:21:07 -0500

    Geolocation

--- JS/geosearch.js
+++ JS/geosearch.js
@@ -1,72 +1,68 @@
 var map;
 var service;
 var infowindow;
-var initialLocation;
 var browserSupportFlag =  new Boolean();
+var requestLocation;
 
-
-function initialize() {
-
-  /*=== Initialize the map ===*/
-  initialLocation = new google.maps.LatLng(42.053317, -87.672788);
-
-  var myOptions = {
-    zoom: 6,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: initialLocation
+function getUserLocation(){
+  if(navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(requestSalonInformation);
   }
-  var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-
-  /*=== Get the user's location ===*/
-
-  // // Try W3C Geolocation (Preferred)
-  // if(navigator.geolocation) {
-  //   browserSupportFlag = true;
-  //   navigator.geolocation.getCurrentPosition(function(position) {
-  //     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-  //     map.setCenter(initialLocation);
-  //   }, function() {
-  //     handleNoGeolocation(browserSupportFlag);
-  //   });
-  // }
-  // // Browser doesn't support Geolocation
-  // else {
-  //   browserSupportFlag = false;
-  //   handleNoGeolocation(browserSupportFlag);
-  // }
-
-
-/*=== Create a places request ===*/
-  
-  var request = {
-    location: initialLocation,
-    types: ['hair_care'],
-    // radius: '10000',
-    // rankBy: google.maps.places.RankBy.PROMINENCE
-    rankBy: google.maps.places.RankBy.DISTANCE
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  else
+  {
+    console.log("Handle no geolocation 1");
+      browserSupportFlag = false;
+      handleNoGeolocation(browserSupportFlag);
+  }
 }
 
-function callback(results, status) {
+function requestSalonInformation(position){
+  if(position)
+  {
+  requestLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var myOptions = {
+              zoom: 6,
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              center: requestLocation
+            }
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+  map.setCenter(requestLocation);
+  console.log("Requesting Location is: "+ requestLocation);
+  var request = {
+                  location : requestLocation,
+                  types: ['hair_care'],
+                  rankBy: google.maps.places.RankBy.DISTANCE
+                };
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, getResultDetails);
+  }
+  else
+  {
+    console.log("Handle no geolocation");
+    handleNoGeolocation(true);
+  }
+}
+
+function getResultDetails(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
         var request = {
             reference: results[i].reference
         }
-        service.getDetails(request, callbackTwo);
+        service.getDetails(request, getResultDistance);
     }
   }
 }
 var distanceService = new google.maps.DistanceMatrixService();
 
-function callbackTwo(place, status) {
+function getResultDistance(place, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
         distanceService.getDistanceMatrix(
       {
-        origins: [initialLocation],
+        origins: [requestLocation],
         destinations: [place.geometry.location],
         travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.IMPERIAL
@@ -114,31 +110,22 @@ function insertPic(data) {
     } 
 }
 
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
 
-/* === map generation (later) === */
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
 
-// function createMarker(place) {
-//   var placeLoc = place.geometry.location;
-//   var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//   });
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
 
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//   });
-// }
 
-  // function handleNoGeolocation(errorFlag) {
-  //   if (errorFlag == true) {
-  //     alert("Geolocation service failed.");
-  //     initialLocation = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-  //   } else {
-  //     alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-  //     initialLocation = new google.maps.LatLng(60, 105);
-  //   }
-  //   map.setCenter(initialLocation);
-  // }
-
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', getUserLocation);
