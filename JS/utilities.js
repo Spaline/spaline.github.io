9af43eccb3003596beb30927437af6fcafb998c6
Amanda <amanda.anumba@gmail.com>
2014-05-16 20:19:37 -0500

    update reservation page alerts

--- JS/utilities.js
+++ JS/utilities.js
@@ -106,10 +106,9 @@ function getURLParams(sParam){
 }
 
 function bookAppointment(){
-
-	var frm = document.getElementById('reservation-form');
-	var fName = frm.firstname.value;
-	var lName = frm.lastname.value;
+	// var frm = document.getElementById('reservation-form');
+	var fName = $("input[name=firstname]").val();
+	var lName = $("input[name=lastname]").val();
 	var rName = fName + " " + lName;
 	var rDate = getURLParams('date');
 	var rTime = getURLParams('time');
@@ -117,12 +116,18 @@ function bookAppointment(){
 	var rAddress = getURLParams('address');
 	var stylist_id = getURLParams('stylistid');
 	var sid = getURLParams('id');
-	var rEmail = String(frm.email.value);
-	var rPhone = String(frm.phone.value);
+	var f_email = $("input[name=email]").val();
+	var rEmail = String(f_email);
+	var f_phone = $("input[name=phone]").val();
+	var rPhone = String(f_phone);
+
+
 	//check to make sure parameters are from url
 	if(!rTime || !rDate || !rSalon || !rAddress || !sid || !stylist_id)
 	{
-		alert("No time, date, or salon selected");
+		$('#alert3').show();
+        setTimeout( "jQuery('#alert3').hide();", 7000 );
+		// alert("No time, date, or salon selected");
 		console.log("Time is: ", rTime);
 		console.log("Date is: ", rDate);
 		console.log("Salon is: ", rSalon);
@@ -131,12 +136,16 @@ function bookAppointment(){
 		console.log("Stylist ID is: ", stylist_id);
 		return;
 	}
+
 	//check to make sure form is completely filled out
 	if(rPhone == "" || rEmail =="" || fName == "" || lName == "")
 	{	
-		alert("There was an error submitting your reservation.\nPlease make sure the form is filled out completely and try submitting again.");
+		// alert("There was an error submitting your reservation.\nPlease make sure the form is filled out completely and try submitting again.");
+		$('#alert3').show();
+        setTimeout( "jQuery('#alert3').hide();", 7000 );
 		return;
 	}
+
 	//create json object
 	var jsonOb = { name: rName,
 				   email: rEmail,
@@ -153,7 +162,11 @@ function bookAppointment(){
 		success: function(confirm){
 			console.log("email sent");
 			console.log(confirm);
-			alert("You have made your reservation! Check your email for confirmation.");
+			$('.jumbotron').hide();
+            $('#reservation-form').hide();
+            $('#s-alert1').show();
+            setTimeout( "jQuery('#s-alert1').hide(); window.location.replace('index.html');", 5000 );
+			// alert("You have made your reservation! Check your email for confirmation.");
 			return;
 		},
 		error: function(error){
@@ -163,13 +176,16 @@ function bookAppointment(){
 			//check to see if it was an invalid email address
 			if(eMessage.indexOf("valid email") > -1)
 			{
-				alert("Your email address is not valid. Check to make sure and try again");
+				$('#alert1').show();
+                setTimeout( "jQuery('#alert1').hide();", 7000 );
+				// alert("Your email address is not valid. Check to make sure and try again");
 				return;
 			}
 			else
-			{
-				
-				alert("There was an error reserving your time slot.");
+			{	
+				$('#alert2').show();
+                setTimeout( "jQuery('#alert2').hide();", 7000 );
+				// alert("There was an error reserving your time slot.");
 				return;
 			}
 		}
